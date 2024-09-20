1d3d3ef99e6eac355b9131c2876005c95b1b900f
Tod Hansmann <tod.hansmann@discordapp.com>
2024-09-20 16:43:59 -0600

    feat: just use a raw TCP connection and dump the request as it comes in

--- main.go
+++ main.go
@@ -1,16 +1,88 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
+	"net"
 	"net/http"
 	"net/http/httputil"
 
 	"github.com/urfave/cli/v2"
 )
+
+type ConnServer struct {
+	port string
+	host string
+}
+
+func NewServer(host, port string) *ConnServer {
+	s := ConnServer{host: host, port: port}
+	if port == "" {
+		s.port = "7780"
+	}
+	return &s
+}
+
+// Attempt to start the server, and if we can't, exit because what else is there?
+func (s *ConnServer) StartServer() {
+	listen, err := net.Listen("tcp", s.host+":"+s.port)
+	if err != nil {
+		log.Fatal("Listening has failed: ", err)
+	}
+
+	// We can return immediately and do other stuff if we want.
+
+	func() {
+		for {
+			conn, err := listen.Accept()
+
+			if err != nil {
+				log.Println("Failed to accept new connection: ", err)
+			}
+			go DumpRequest(conn)
+		}
+	}()
+}
+
+func DumpRequest(conn net.Conn) {
+	t := time.Now()
+	tfmt := t.Format("20060102150405")
+	thttpfmt := t.Format(http.TimeFormat)
+	defer conn.Close()
+
+	f, err := os.CreateTemp("", "gowebdump-"+tfmt+"-") // in Go version older than 1.17 you can use ioutil.TempFile
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(conn)
+
+	response := []byte("HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\nServer: gowebdump 0.1\nDate: " + thttpfmt + "Content-Length: 2\n\nOK")
+
+	for {
+		bytes, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Write whatever every time, as we want the raw request as is
+		line := string(bytes)
+		if _, err := f.Write([]byte(line + "\n")); err != nil {
+			log.Fatal(err)
+		}
+		if line == "" {
+			// We have an empty line, request over
+			break
+		}
+	}
+
+	fmt.Printf("%s\n", f.Name())
+	conn.Write(response)
+}
 
 func DmpRoot(w http.ResponseWriter, r *http.Request) {
 	dump, err := httputil.DumpRequest(r, true)
@@ -18,7 +90,7 @@ func DmpRoot(w http.ResponseWriter, r *http.Request) {
 		t := time.Now()
 		tfmt := t.Format("20060102150405")
 		f, err := os.CreateTemp("", "gowebdump-"+tfmt+"-") // in Go version older than 1.17 you can use ioutil.TempFile
-        fmt.Printf("%s\n", f.Name())
+		fmt.Printf("%s\n", f.Name())
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -52,7 +124,8 @@ func main() {
 				Aliases: []string{"s"},
 				Usage:   "serve stuff",
 				Action: func(cCtx *cli.Context) error {
-					Serve()
+					server := NewServer("0.0.0.0", "7780")
+					server.StartServer()
 					return nil
 				},
 			},
